8e03996dec6ad5fb25c268783cc31dfad0f6401c
Anthony Dervish <ant@Anthonys-iMac.local>
2017-01-29 19:42:46 +0000

    Change to use stow

--- setup_env.sh
+++ setup_env.sh
@@ -16,25 +16,20 @@ set -o nounset                              # Treat unset variables as an error
 ScriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
 cd "$ScriptDir"
 
-RcFiles=$(find . -maxdepth 1 -name '.*' | tail -n +2)
-
-cd $HOME
-
-for f in ${RcFiles}; do
-    RcFile=${f#./}
-
-	[[ "$RcFile" = ".git" ]] && continue
-	
-    if [[ -e "$f" ]]; then
-        echo "Moving ~/$RcFile to ~/$RcFile.moved"
-        mv "$RcFile" "$RcFile.moved"
-    fi
-
-    ln -s "$ScriptDir/$RcFile" $RcFile
-done
-
-if [[ ! -d "profile.d" ]]; then
-    ln -s "$ScriptDir/profile.d" .
+if ! [[ -d bin/stow ]]; then
+    cd bin
+    tar zxvf stow.tgz
+    cd ..
 fi
+
+Stow=bin/stow/stow
+
+RcFiles=$(find . -maxdepth 1 -type d -name '*-config')
+
+for ConfigDir in ${RcFiles}; do
+    ConfigDir=${ConfigDir#./}
+    echo "Install ${ConfigDir}"
+    $Stow -v --target="${HOME}" -S "${ConfigDir}"
+done
 
 # vim:sts=4:ts=4:sw=4:
